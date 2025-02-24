68830c4f7b4e4614be0426a5029c1f3bad57b2bb
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2025-02-24 16:55:55 +0000

    Remove challenge link

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,6 @@ function App() {
     <>
       <h1>Samuel's Portfolio</h1>
       <h2>Under construction...</h2>
-      <p>Are you looking for the CTF challenge? <a href="challenge">Click here</a></p>
       <a href="https://github.com/Samuel-BlankAmber/portfolio" target="_blank" rel="noopener noreferrer">
         This site is open source!
       </a>
