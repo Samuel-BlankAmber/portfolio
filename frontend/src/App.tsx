3a3e9126c0183267b7ec5effd4bd5634e94d681b
BlankAmber <145449058+BlankAmber@users.noreply.github.com>
2025-02-28 18:03:49 +0000

    Add potential redesign

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Intro from "./pages/Intro";
-import Home from "./pages/Home";
+// import Intro from "./pages/Intro";
+// import Home from "./pages/Home";
+import HomeRedesign from "./pages/HomeRedesign";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Intro />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/" element={<Intro />} /> */}
+        {/* <Route path="/home" element={<Home />} /> */}
+        <Route path="/home" element={<HomeRedesign />} />
       </Routes>
     </BrowserRouter>
   );
